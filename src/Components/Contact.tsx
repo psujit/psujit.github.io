e20562693df74d195a40f7b0bd0c79d6ecbfec98
Sujit <psujit14@gmail.com>
2019-04-07 22:30:55 +0200

    Make mailto possible

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -5,10 +5,73 @@ interface DataProps {
   data: Main;
 };
 
-class Contact extends React.Component<DataProps> {
-  handleChange() {
-    return;
+interface ContactFormState {
+  classForButton?: string;
+  contactEmail?: string;
+  contactMessage?: string;
+  contactName?: string;
+  contactSubject?: string;
+};
+
+class Contact extends React.Component<DataProps, ContactFormState> {
+  constructor(props: DataProps) {
+    super(props);
+    this.state = {
+      classForButton: 'button isDisabled',
+      contactEmail: '',
+      contactMessage: '',
+      contactName: '',
+      contactSubject: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
   }
+
+  handleChange = async (event: React.FormEvent) => {
+    let target;
+    switch(event.currentTarget.id) {
+
+      case 'contactName': {
+        target  = event.target as HTMLInputElement;
+        await this.setState({
+        contactName: target.value
+        })
+       break;
+      }
+
+     case 'contactEmail': {
+      target  = event.target as HTMLInputElement;
+      await this.setState({
+        contactEmail: target.value
+      })
+      break;
+     }
+
+     case 'contactSubject': {
+      target  = event.target as HTMLInputElement;
+      await this.setState({
+        contactSubject: target.value
+      })
+      break;
+     }
+
+     case 'contactMessage': {
+      target  = event.target as HTMLTextAreaElement;
+      await this.setState({
+        contactMessage: target.value
+      })
+      break;
+     }
+
+     default: break;
+    };
+
+    if(this.state.contactName && this.state.contactEmail && this.state.contactMessage) {
+      this.setState({
+        classForButton: 'button isActive'
+      })
+    }
+  }
+  
   render() {
     const { email, name, phone } = this.props.data;
     const { city, state, street, zip } = this.props.data.address;
@@ -40,37 +103,34 @@ class Contact extends React.Component<DataProps> {
         <div className="row">
           <div className="eight columns">
 
-            <form action="" method="post" id="contactForm" name="contactForm">
               <fieldset>
-
                 <div>
                   <label htmlFor="contactName">Name <span className="required">*</span></label>
-                  <input type="text" defaultValue="" size={size} id="contactName" name="contactName" required={true} onChange={this.handleChange} />
+                  <input type="text" defaultValue="" size={size} id="contactName" name="contactName" required={true} onChange={ this.handleChange } />
                 </div>
 
                 <div>
                   <label htmlFor="contactEmail">Email <span className="required">*</span></label>
-                  <input type="text" defaultValue="" size={size} id="contactEmail" name="contactEmail" required={true} onChange={this.handleChange} />
+                  <input type="text" defaultValue="" size={size} id="contactEmail" name="contactEmail" required={true} onChange={ this.handleChange } />
                 </div>
 
                 <div>
                   <label htmlFor="contactSubject">Subject</label>
-                  <input type="text" defaultValue="" size={size} id="contactSubject" name="contactSubject" onChange={this.handleChange} />
+                  <input type="text" defaultValue="" size={size} id="contactSubject" name="contactSubject" onChange={ this.handleChange } />
                 </div>
 
                 <div>
                   <label htmlFor="contactMessage">Message <span className="required">*</span></label>
-                  <textarea cols={cols} rows={rows} id="contactMessage" required={true} name="contactMessage" />
+                  <textarea cols={cols} rows={rows} id="contactMessage" required={true} name="contactMessage" onChange={ this.handleChange } />
                 </div>
 
                 <div>
-                  <a className="submit" href={"mailto:" + email + ""}>Submit</a>
+                  <a className={this.state.classForButton} href={"mailto:" + email + "?subject=" + this.state.contactSubject + "&body=" + this.state.contactMessage+this.state.contactName}>Submit</a>
                   <span id="image-loader">
                     <img alt="" src="images/loader.gif" />
                   </span>
                 </div>
               </fieldset>
-            </form>
 
             <div id="message-warning"> Error boy</div>
             <div id="message-success">
